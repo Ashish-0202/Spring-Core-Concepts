d58b00e9f4aa625172c388949424159bb5d10a3c
Ashish Shenoy <120568661+Ashish200202@users.noreply.github.com>
2024-03-15 17:55:16 +0530

    Injection of Dependency with Multiple beans

---
+++ InjectionWithMultipleBeans/src/main/java/com/springcore/InjectionWithMultipleBeans/InjectionWithMultipleBeansApplication.java
@@ -0,0 +1,13 @@
+package com.springcore.InjectionWithMultipleBeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectionWithMultipleBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjectionWithMultipleBeansApplication.class, args);
+	}
+
+}
---
+++ InjectionWithMultipleBeans/src/test/java/com/springcore/InjectionWithMultipleBeans/InjectionWithMultipleBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcore.InjectionWithMultipleBeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjectionWithMultipleBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
